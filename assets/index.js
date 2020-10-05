43ff5944a3adb23dd0a9a0432c74e53a5f26c19b
Joshua Duncan <joshuaduncan@Joshuas-MacBook-Pro.local>
2020-10-04 18:02:23 -0600

    working on the timer

--- assets/index.js
+++ assets/index.js
@@ -2,26 +2,36 @@ var start = document.getElementById("startBtn")
 var score = document.getElementById("scoreDiv")
 var timeLeft = document.getElementById("timeDiv")
 var question = document.getElementById("questionDiv")
-var answers = document.getElementById("answersDiv")
-var questions = {
-    one : "What is Javascript",
-    two : "What does a function do",
-    three : "What am I",
-    four : "",
-    five : ""
-}
+var answer = document.getElementById("answersDiv")
+var questions = { one: "What is Javascript", two: "What does a function do", three: "What am I", four: "", five: "" }
+var answersOne = [];
+var answersTwo = [];
+var answersThree = [];
+var answersFour = [];
+var answersFive = [];
+var secondsLeft = 10
 
-function startQuiz (){
+
+
+function startQuiz() {
     score.textContent = 0;
-    timeLeft.textContent = 10;
+    timeLeft.textContent = secondsLeft;
     question.textContent = questions.one
     document.getElementById("quizDiv").removeAttribute("class")
     document.getElementById("openDiv").setAttribute("class", "hidden")
+    countdown()
 
+    function countdown() {
+        var downloadTimer = setInterval(function () {
+            if (secondsLeft <= 0) {
+                clearInterval(downloadTimer);
+                alert("time's up")
+            }
+            timeLeft.textContent = secondsLeft--;
+            console.log(secondsLeft)
+        }, 1000);
+    }
 }
-
-
-
 
 
 start.addEventListener("click", startQuiz)
